8d3bb8f990d4726db8d5e6a7f481173a25ca8e78
OG-Jons <jonas@marschall.pro>
2022-05-13 10:45:35 +0200

    good video link

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -5,7 +5,7 @@
     <iframe
       width="1008"
       height="576"
-      src="https://www.youtube.com/embed/hddqFkYxhMQ"
+      src="https://www.youtube.com/embed/ZnYti6OrFzc"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
