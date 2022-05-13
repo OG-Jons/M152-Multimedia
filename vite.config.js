9f2a2f4317a2ef930e2d168e781847568f39868b
OG-Jons <jonas@marschall.pro>
2022-05-13 11:06:57 +0200

    fixed build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ["1.png", "2.png", "3.png"],
+    },
+  },
+});
