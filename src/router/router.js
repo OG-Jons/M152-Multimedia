62cbf35efd8782d3c90ab3aba389d3121cc9df9f
TAAMAJOM <jonas.marschall@swisscom.com>
2022-05-20 10:21:35 +0200

    Disabled history mode

--- src/router/router.js
+++ src/router/router.js
@@ -1,29 +1,29 @@
-import {createWebHistory, createRouter} from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import LandingPage from "../views/LandingPage.vue";
 import Scrolling from "../views/Scrolling.vue";
 import VideoView from "../views/VideoView.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: LandingPage,
-    },
-    {
-        path: "/scroll",
-        name: "Scrolling animations",
-        component: Scrolling,
-    },
-    {
-        path: "/video",
-      name: "Backrooms Video",
-        component: VideoView,
-    }
+  {
+    path: "/",
+    name: "home",
+    component: LandingPage,
+  },
+  {
+    path: "/scroll",
+    name: "Scrolling animations",
+    component: Scrolling,
+  },
+  {
+    path: "/video",
+    name: "Backrooms Video",
+    component: VideoView,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHashHistory(),
+  routes,
 });
 
 export default router;
